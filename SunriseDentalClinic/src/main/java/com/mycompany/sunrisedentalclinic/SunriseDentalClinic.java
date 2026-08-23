3dd4b1214179a834bfa22d993ddada57135f43c1
Dinuli Disara <dinulidisara99@gmail.com>
2026-08-24 00:53:04 +0530

    Initial Maven project

---
+++ SunriseDentalClinic/src/main/java/com/mycompany/sunrisedentalclinic/SunriseDentalClinic.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sunrisedentalclinic;
+
+/**
+ *
+ * @author Dinuli Disara
+ */
+public class SunriseDentalClinic {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
